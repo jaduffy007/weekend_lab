7d164cd4c22158b4597cbb5f5dc223aa931039a4
JamesDuffy <jaduffy007@gmail.com>
2014-10-03 16:18:30 -0700

    weekend_lab

--- tic_tac.js
+++ tic_tac.js
@@ -13,7 +13,7 @@ var player2 = {
 
 var selector = function () {
 	if (counter%2 === 0) {
-		var selectedmarker = player1.marker1;
+		var selectedmarker = player1.marker;
 	}else{
 			selectedmarker = player2.marker;
 	}
@@ -23,6 +23,7 @@ var selector = function () {
 var counter = 0;
 var magic = function () {
 	counter++;
+	selector;
 	};
 
 
@@ -30,13 +31,13 @@ var magic = function () {
 var initialize = function () {
 
 	document.querySelector("#slot0").addEventListener("click",magic);
-	document.querySelector("#slot1").addEventListener("click",someFunction);
-	document.querySelector("#slot2").addEventListener("click",someFunction);
-	document.querySelector("#slot3").addEventListener("click",someFunction);
-	document.querySelector("#slot4").addEventListener("click",someFunction);
-	document.querySelector("#slot5").addEventListener("click",someFunction);
-	document.querySelector("#slot6").addEventListener("click",someFunction);
-	document.querySelector("#slot7").addEventListener("click",someFunction);
-	document.querySelector("#slot8").addEventListener("click",someFunction);
+	document.querySelector("#slot1").addEventListener("click",magic);
+	document.querySelector("#slot2").addEventListener("click",magic);
+	document.querySelector("#slot3").addEventListener("click",magic);
+	document.querySelector("#slot4").addEventListener("click",magic);
+	document.querySelector("#slot5").addEventListener("click",magic);
+	document.querySelector("#slot6").addEventListener("click",magic);
+	document.querySelector("#slot7").addEventListener("click",magic);
+	document.querySelector("#slot8").addEventListener("click",magic);
 };
 
